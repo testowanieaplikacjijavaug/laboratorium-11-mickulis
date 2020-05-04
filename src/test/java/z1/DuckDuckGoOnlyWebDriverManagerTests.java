c5642043170a209846aed48e130d30ef29e3a8c6
mickulis <mkulis@sigma.ug.edu.pl>
2020-05-04 09:07:56 +0200

    z1

---
+++ src/test/java/z1/DuckDuckGoOnlyWebDriverManagerTests.java
@@ -0,0 +1,59 @@
+package z1;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NotFoundException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DuckDuckGoOnlyWebDriverManagerTests
+{
+	WebDriver driver;
+	DuckDuckGoPage page;
+
+
+	@BeforeAll
+	public static void oneTimeSetup()
+	{
+		WebDriverManager.chromedriver().setup();
+	}
+
+	@BeforeEach
+	public void setup()
+	{
+		driver = new ChromeDriver();
+		page = new DuckDuckGoPage(driver);
+	}
+
+	@AfterEach
+	public void teardown()
+	{
+		driver.quit();
+	}
+
+	@Test
+	public void searchForSomethingAndOpenFirstResult()
+	{
+		page.searchFor("duck").clickOnNthResult(1);
+	}
+
+	@Test
+	public void searchForSomethingAndOpenThirdResult()
+	{
+		page.searchFor("duck").clickOnNthResult(3);
+	}
+
+	@Test
+	public void searchForSomethingAndOpenNonExistingResult()
+	{
+		int numberOfResults = page.searchFor("duck").getNumberOfResults();
+		Assertions.assertThrows(NotFoundException.class, () -> page.clickOnNthResult(numberOfResults+1));
+	}
+
+	@Test
+	public void findSomethingThatDoesntExist()
+	{
+		Assertions.assertThrows(NotFoundException.class, () -> driver.findElement(By.linkText("asdfghjhgfdsasdfgh")));
+	}
+}
---
+++ src/test/java/z1/DuckDuckGoSeleniumExtensionTests.java
@@ -0,0 +1,62 @@
+package z1;
+
+import io.github.bonigarcia.seljup.SeleniumExtension;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NotFoundException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+@ExtendWith(SeleniumExtension.class)
+public class DuckDuckGoSeleniumExtensionTests
+{
+	WebDriver driver;
+	DuckDuckGoPage page;
+
+	public DuckDuckGoSeleniumExtensionTests(ChromeDriver driver)
+	{
+		this.driver = driver;
+	}
+
+	@BeforeEach
+	public void setup()
+	{
+		page = new DuckDuckGoPage(driver);
+	}
+
+	@AfterEach
+	public void teardown()
+	{
+		driver.quit();
+	}
+
+	@Test
+	public void searchForSomethingAndOpenFirstResult()
+	{
+		page.searchFor("duck").clickOnNthResult(1);
+	}
+
+	@Test
+	public void searchForSomethingAndOpenThirdResult()
+	{
+		page.searchFor("duck").clickOnNthResult(3);
+	}
+
+	@Test
+	public void searchForSomethingAndOpenNonExistingResult()
+	{
+		int numberOfResults = page.searchFor("duck").getNumberOfResults();
+		Assertions.assertThrows(NotFoundException.class, () -> page.clickOnNthResult(numberOfResults+1));
+	}
+
+	@Test
+	public void findSomethingThatDoesntExist()
+	{
+		Assertions.assertThrows(NotFoundException.class, () -> driver.findElement(By.linkText("asdfghjhgfdsasdfgh")));
+	}
+}
